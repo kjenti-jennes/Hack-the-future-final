8802717c9919ea6c4122f4740772a83b9e623653
kjenti-jennes <62223824+kjenti-jennes@users.noreply.github.com>
2020-11-24 16:49:53 +0100

    ReverseString

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public void ReverseString(String input) {
+        StringBuilder input1 = new StringBuilder();
+
+        input1.append(input);
+        input1 = input1.reverse();
+
+        System.out.println(input1);
+
+    }
+}
